78a0792c0fca35fd09e32ce3f96e99a6c1595013
zyy <1755635168@qq.com>
2022-08-10 11:01:34 +0800

    version3.4

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int main(int argc, char **argv)
         Ir.deadExpDelete();
         //Ir.loadDelete();
     }
-    Ir.loadDelete();
+    //Ir.loadDelete();
     //Ir.printIRs(true);
     Assembler assembler = Assembler(Ir);
     assembler.generateAsm();
